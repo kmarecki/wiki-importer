3dafc58b4ea6f8d0f9fa3101390714a8991aef5c
Krzysztof Marecki <marecki.krzysztof@gmail.com>
2016-05-14 15:52:28 +0200

    Add WikiParser

--- app.ts
+++ app.ts
@@ -1,10 +1,12 @@
 /// <reference path="typings/main.d.ts" />
 
+process.env.UV_THREADPOOL_SIZE = 8;
+
 import * as minimist from 'minimist';
 import {Splitter} from './splitter';
 
 var argv = minimist(process.argv.slice(2));
 console.log(argv);
 
-var splitter = new Splitter();
-splitter.split(argv['xmlPath'], argv['outputDir']);
+var splitter = new Splitter(argv['xmlPath'], argv['outputDir'], argv._);
+splitter.split();
--- splitter.ts
+++ splitter.ts
@@ -1,7 +1,10 @@
 import * as _ from 'lodash';
 import * as sax from 'sax';
-import * as fs from 'fs';
+import * as fs from 'fs-extra';
+import * as path from 'path';
 import * as util from 'util';
+
+var readlineSync = require('readline-sync');
 
 class Page {
     title: string;
@@ -22,7 +25,7 @@ class SaxObjectReader {
     private get currentObject(): Object {
         return _.last(this.objects);
     }
-    
+
     private get rootObject(): Object {
         return _.first(this.objects);
     }
@@ -38,9 +41,9 @@ class SaxObjectReader {
             this.tags = [tag.name];
         } else {
             var propertyName = tag.name;
-            Object.defineProperty(this.currentObject, propertyName, { 
-                enumerable: true, 
-                writable: true, 
+            Object.defineProperty(this.currentObject, propertyName, {
+                enumerable: true,
+                writable: true,
                 value: new Object
             });
             this.tags.push(propertyName);
@@ -55,19 +58,19 @@ class SaxObjectReader {
     }
 
     private reduceObject(object: Object): void {
-        for(var propertyName of Object.getOwnPropertyNames(object)) {
+        for (var propertyName of Object.getOwnPropertyNames(object)) {
             var childObject = object[propertyName];
             if (Object.getOwnPropertyNames(childObject).length == 1) {
                 object[propertyName] = childObject["#text"];
             }
         }
     }
-    
+
     closeTag(tag: string): void {
         if (tag) {
             if (this.currentTag == tag) {
                 this.tags.pop();
-                 
+
                 this.reduceObject(this.currentObject);
                 this.objects.pop();
             } else {
@@ -82,13 +85,92 @@ class SaxObjectReader {
     }
 }
 
+class TextPart {
+    value: string;
+    text: string;
+}
+
+class WikiTemplateParser {
+
+    splitH2(text: string): TextPart[] {
+        var parts: TextPart[] = [];
+        var currentPart: TextPart;
+        for (var line of text.split('\n')) {
+            if (line.match(/^==[^=\|[\n\r\t.,'\"\+!?-]+==/g)) {
+                if (currentPart != null) {
+                    parts.push(currentPart);
+                }
+                currentPart = new TextPart();
+                currentPart.value = line.replace(/=/g, '').trim();
+                currentPart.text = line;
+            } else {
+                if (currentPart != null) {
+                    currentPart.text = currentPart.text + '\n' + line;
+                }
+            }
+        }
+        if (currentPart != null) {
+            parts.push(currentPart);
+        }
+        return parts;
+    }
+}
+
 export class Splitter {
     private currentTag: sax.Tag;
+    private xmlPath: string;
+    private outputDir: string;
+    private languages: string[];
+    private tagCount: number;
 
-    split(xmlPath: string, outputDir: string): void {
+    private wikiParser = new WikiTemplateParser();
+
+    constructor(xmlPath: string, outputDir: string, languages: string[]) {
+        this.xmlPath = xmlPath;
+        this.outputDir = outputDir;
+        this.languages = languages;
+    }
+
+    private savePage(raw: any): void {
+        if (raw.ns == 0) {
+            if (raw.title) {
+                try {
+                    for (var language of this.wikiParser.splitH2(raw.revision.text)) {
+                        if (this.languages.length == 0 || _.includes(this.languages, language.value)) {
+                            let page = new Page();
+                            page.title = raw.title.replace('/', '_');
+                            page.revisionId = raw.revision.id;
+                            page.timestamp = raw.revision.timestamp;
+                            page.text = language.text;
+
+                            let filename = path.join(this.outputDir, language.value, page.title);
+                            let dirname = path.dirname(filename);
+                            console.log(`${this.tagCount} ${page.title} - ${language.value}, ${filename} ${dirname}`);
+
+                            fs.ensureDir(dirname, err => fs.writeFile(filename, JSON.stringify(page, null, 4)));
+                        }
+                        this.tagCount++;
+                    }
+                } catch (e) {
+                    let errorfilename = path.join(this.outputDir, '_errors', (this.tagCount++).toString());
+                    let data = {
+                        error: { message: e.message, stack: e.stack },
+                        raw: raw
+                    }
+                    console.log(e.name + ': ' + e.message);
+                    console.log(errorfilename);
+                    readlineSync.question('');
+                    let dirname = path.dirname(errorfilename);
+                    fs.ensureDir(dirname, err => fs.writeFile(errorfilename, JSON.stringify(data, null, 4)));
+                }
+            }
+        }
+    }
+
+    split(): void {
         var reader: SaxObjectReader;
         var saxStream = sax.createStream(true, {});
-        var tagCount = 1;
+        this.tagCount = 1;
         saxStream.on('error', function (e) {
             console.error('error!', e);
             this._parser.error = null;
@@ -106,7 +188,7 @@ export class Splitter {
                 reader.openTag(tag);
             }
         });
-        saxStream.on('text', text => {    
+        saxStream.on('text', text => {
             if (reader) {
                 text = text.trim();
                 reader.setTagText(text);
@@ -115,19 +197,19 @@ export class Splitter {
         saxStream.on('closetag', tag => {
             this.currentTag = null;
             if (tag == 'page') {
-                var page = reader.toObject();
-                console.log(JSON.stringify(page));
+                var raw = <any>reader.toObject();
+                this.savePage(raw);
             }
             if (reader) {
                 reader.closeTag(tag);
             }
         });
-       
-        fs.exists(xmlPath, (exists) => {
+
+        fs.exists(this.xmlPath, (exists) => {
             if (!exists) {
-                console.log(xmlPath + ' not exist');
+                console.log(this.xmlPath + ' not exist');
             }
         });
-        fs.createReadStream(xmlPath).pipe(saxStream);
+        fs.createReadStream(this.xmlPath).pipe(saxStream);
     }
 }
